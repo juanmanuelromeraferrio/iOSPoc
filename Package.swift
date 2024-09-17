ebb6b190d346dbb03c9571cf4fd8f9b5f6fe6b7b
Juan Manuel Romera <juanmanuel.romeraferrio@gmail.com>
2024-09-17 16:12:44 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPOC",
+    targets: [
+        .executableTarget(
+            name: "SwiftPOC",
+            path: "Sources")
+    ]
+)
