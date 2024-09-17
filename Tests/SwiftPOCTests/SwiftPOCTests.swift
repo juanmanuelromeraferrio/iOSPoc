3dabefdacdc8460a6c2a390d637a3eccbe99c49f
Juan Manuel Romera <juanmanuel.romeraferrio@gmail.com>
2024-09-17 16:17:48 +0200

    Create SwiftPOCTests.swift

---
+++ Tests/SwiftPOCTests/SwiftPOCTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import SwiftPOC
+
+final class SwiftPOCTests: XCTestCase {
+    func testExample() {
+        // Example test case
+        XCTAssertEqual(2 + 2, 4, "Math works!")
+    }
+}
